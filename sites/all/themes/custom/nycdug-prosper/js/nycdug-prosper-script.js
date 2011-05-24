9e4c1a90506969f1e64cf6fa9f380db431fef503
Kelly Bell <kelly@gothamcitydrupal.com>
2011-05-23 21:09:47 -0400

    deleted unused modules and themes
    Signed-off-by: Kelly Bell <kelly@gothamcitydrupal.com>

--- sites/all/themes/custom/nycdug-prosper/js/nycdug-prosper-script.js
+++
@@ -1,8 +0,0 @@
-// $Id: nycdug-prosper-script.js,v 1.3 2011/04/12 21:36:06 kbell Exp $
-
-Drupal.behaviors.m3_prosperRoundedCorner = function (context) {
-  $(".prosper-rounded-title h2.block-title").corner("top 5px"); 
-  $(".prosper-shoppingcart h2.block-title").corner("top 5px"); 
-  $(".prosper-menu-list h2.block-title").corner("top 5px"); 
-  $(".prosper-grayborder-darkbackground .inner").corner("7px"); 
-};
