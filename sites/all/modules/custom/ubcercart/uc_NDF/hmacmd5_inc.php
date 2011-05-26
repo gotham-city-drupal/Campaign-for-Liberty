2f20750e25eac3b8b5edde8ac1053b72cf85e7b7
Kelly Bell <kelly@gothamcitydrupal.com>
2011-05-26 01:09:55 -0400

    added ubercart_NDF to payment processors (via symlink - the original is in modules/custom
    Signed-off-by: Kelly Bell <kelly@gothamcitydrupal.com>

---
+++ sites/all/modules/custom/ubcercart/uc_NDF/hmacmd5_inc.php
@@ -0,0 +1,22 @@
+<?php
+	function hex_hmac_md5 ($key, $data)
+	{
+	   // RFC 2104 HMAC implementation for php.
+	   // Creates an md5 HMAC.
+	   // Eliminates the need to install mhash to compute a HMAC
+	   // Hacked by Lance Rushing
+
+	   $b = 64; // byte length for md5
+	   if (strlen($key) > $b)
+	   {
+	       $key = pack("H*",md5($key));
+	   }
+	   $key  = str_pad($key, $b, chr(0x00));
+	   $ipad = str_pad('', $b, chr(0x36));
+	   $opad = str_pad('', $b, chr(0x5c));
+	   $k_ipad = $key ^ $ipad ;
+	   $k_opad = $key ^ $opad;
+
+	   return md5($k_opad  . pack("H*",md5($k_ipad . $data)));
+	}
+?>
